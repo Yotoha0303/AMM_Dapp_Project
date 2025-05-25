7ee1dab085b313d0d71e89e38a9c92dfb03dba7d
Yotoha <103411612+Yotoha0303@users.noreply.github.com>
2025-05-25 15:15:14 +0800

    add

--- amm-contract-dev/src/AMMPair.sol
+++ amm-contract-dev/src/AMMPair.sol
@@ -170,7 +170,32 @@ contract AMMPair is IUniswapV2Pair, UniswapV2ERC20 {
     function burn(
         address to
     ) external lock returns (uint amount0, uint amount1) {
-        
+        (uint112 _reserve0, uint112 _reserve1, ) = getReserves();
+        address _token0 = token0;
+        address _token1 = token1;
+        uint balance0 = IERC20(_token0).balanceOf(address(this));
+        uint balance1 = IERC20(_token1).balanceOf(address(this));
+        uint liquididity = balanceOf[address(this)];
+
+        bool feeOn = _mint(_reserve0, _reserve1);
+        uint _totalSupply = totalSupply;
+        amount0 = liquididity.mul(balance0);
+        amount1 = liquididity.mul(balance1);
+        require(
+            amount0 > 0 && amount1,
+            "UniswapV2:INSUFFICIENT_LIQUIDITY_BURNED"
+        );
+        _burn(address(this), liquididity);
+        _safeTransfer(_token0, to, amount0);
+        _safeTransfer(_token1, to, amount1);
+        balance0 = IERC20(_token0).balanceOf(address(this));
+        balance1 = IERC20(_token1).balanceOf(address(this));
+
+        _update(balance0, balance1, _reserve0, _reserve1);
+        if (feeOn) {
+            kLast = uint(reserve0).mul(reserve1);
+        }
+        emit Burn(msg.sender, amount0, amount1, to);
     }
 
     function swap(
@@ -178,9 +203,89 @@ contract AMMPair is IUniswapV2Pair, UniswapV2ERC20 {
         uint amount1Out,
         address to,
         bytes calldata data
-    ) external lock {}
+    ) external lock {
+        require(
+            amount0Out > 0 || amount1Out > 0,
+            "UniswapV2:INSUFFCIENT_OUTPUT_AMOUNT"
+        );
+        (uint112 _reserve0, uint112 _reserve1, ) = getReserves();
+        require(
+            amount0Out < _reserve0 && amount1Out < _reserve1,
+            "UniswapV2:INSUFFCIENT_LIQUIDITY"
+        );
 
-    function skim(address to) external lock {}
+        uint balance0;
+        uint balance1;
 
-    function sync() external lock {}
+        {
+            address _token0 = token0;
+            address _token1 = token1;
+
+            require(to != _token0 && to != _token1, "UniswapV2:INVALID_TO");
+            if (amount0Out > 0) {
+                _safeTransfer(_token0, to, amount0Out);
+            }
+            if (amount1Out > 0) {
+                _safeTransfer(_token1, to, amount1Out);
+            }
+            if (data.length > 0) {
+                IUniswapV2Callee(to).uniswapV2Call(
+                    msg.sender,
+                    amount0Out,
+                    amount1Out,
+                    data
+                );
+            }
+            balance0 = IERC20(_token0).balanceOf(address(this));
+            balance1 = IERC20(_token1).balanceOf(address(this));
+        }
+
+        uint amount0In = balance0 > _reserve0 - amount0Out
+            ? balance0 - (_reserve0 - amount0Out)
+            : 0;
+        uint amount1In = balance1 > _reserve1 - amount1Out
+            ? balance1 - (_reserve1 - amount1Out)
+            : 0;
+
+        require(
+            amount0In > 0 || amount1In > 0,
+            "UniswapV2:INSUFFICIENT_INPUT_AMOUNT"
+        );
+        {
+            uint balance0adjusted = balance0.mul(1000).sub(amount0In.mul(3));
+            uint balance1adjusted = balance1.mul(1000).sub(amount1In.mul(3));
+            require(
+                balance0adjusted.mul(balance1adjusted) >=
+                    uint(_reserve0).mul(_reserve1).mul(1000 ** 2),
+                "UniswapV2:k"
+            );
+        }
+
+        _update(balance0, balance1, _reserve0, _reserve1);
+        emit Swap(msg.sender, amount0In, amount1In, amount0Out, amount1Out, to);
+    }
+
+    function skim(address to) external lock {
+        address _token0 = token0;
+        address _token1 = token1;
+        _safeTransfer(
+            _token0,
+            to,
+            IERC20(_token0).balanceOf(address(this)).sub(reserve0)
+        );
+        _safeTransfer(
+            _token1,
+            to,
+            IERC20(_token1).balanceOf(address(this)).sub(reserve1)
+        );
+    }
+
+    function sync() external lock {
+        _update(
+            IERC20(token0).balanceOf(address(this)),
+            IERC20(token1).balanceOf(address(this)),
+            reserve0,
+            reserve1
+        );
+    }
 }

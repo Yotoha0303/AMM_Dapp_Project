1aad3d2d0ac4ad4929210d0dc3c8ec655141d0ec
Yotoha <103411612+Yotoha0303@users.noreply.github.com>
2025-05-26 23:05:11 +0800

    new AMMToken

---
+++ amm-contract-dev/contracts/AMMToken.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+// 挖矿奖励代币
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract AMMToken{
+    constructor(uint256 initialSupply) ERC20("DEX Token", "DEX") {
+        _mint(msg.sender, initialSupply);
+    }
+}
--- amm-contract-dev/contracts/StakingRewards.sol
+++ amm-contract-dev/contracts/StakingRewards.sol
@@ -2,6 +2,11 @@
 // 挖矿奖励逻辑
 pragma solidity ^0.8.20;
 
+// import "./AMMPair.sol";
+// import "./UniswapV2ERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+
 contract StakingRewards{
     
 }

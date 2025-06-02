07783db30313207e0d8944edb2abc6981a478c57
Yotoha <103411612+Yotoha0303@users.noreply.github.com>
2025-06-02 23:07:28 +0800

    updata

---
+++ amm-contract-dev/script/Deploy.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "forge-std/Script.sol";
+
+contract Deploy is Script{
+
+    function setUp() public{
+
+    }
+
+    function run() public{
+        
+    }
+}
--- amm-contract-dev/test/UniswapV2ERC20.t.sol
+++ amm-contract-dev/test/UniswapV2ERC20.t.sol
@@ -2,13 +2,12 @@
 
 pragma solidity ^0.8.20;
 
-import "../contracts/UniswapV2ERC20.t.sol";
+import "../contracts/UniswapV2ERC20.sol";
 import "forge-std/Test.sol";
 
 contract UniswapV2ERC20 is Test {
 
     function setUp(){
-        // new aToken(0);
 
     }
 }
